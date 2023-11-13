5db0b68ae91af1ee0d4f85f65f8c6dc1139bcacd
James Hulena <63756285+guesswhoitis@users.noreply.github.com>
2023-11-13 16:25:06 +1300

    Update code to send across file type correctly Replace + with - in url to stop corruption in ray.so

--- src/main/kotlin/com/raysolink/RaySoLink.kt
+++ src/main/kotlin/com/raysolink/RaySoLink.kt
@@ -1,14 +1,27 @@
+package com.raysolink
+
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiDocumentManager
+import io.ktor.util.*
+import java.awt.Desktop
 import java.net.URI
 import java.util.*
-import java.awt.Desktop
-import kotlin.jvm.Throws
+
 
 class RaySoLink : AnAction() {
+
+    fun getFileLanguage(fileType: FileType): String {
+        val name = fileType.name
+        //if ide does not have correct file type for ray.so can add an exception in here
+        return when(name) {
+            else -> name.toLowerCasePreservingASCIIRules()
+        }
+    }
+
     @Throws(Exception::class)
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project
@@ -18,15 +31,29 @@ class RaySoLink : AnAction() {
             return
         }
 
-        val selectedCode = editor.selectionModel.selectedText
-        if (selectedCode == null || selectedCode.isEmpty()) {
+        val selectedCode = editor.selectionModel.selectedText.toString()
+        if (selectedCode.isEmpty()) {
             Messages.showMessageDialog(project, "No code selected", "Error", Messages.getErrorIcon())
             return
         }
 
         val base64Code = Base64.getEncoder().encodeToString(selectedCode.toByteArray())
 
-        val url = "https://ray.so/#language=php&theme=raindrop&padding=16&background=true&code=$base64Code"
+        val psiFile = PsiDocumentManager.getInstance(project!!).getPsiFile(editor.document)
+
+        var fileType: FileType? = null
+        if (psiFile != null) {
+            fileType = psiFile.fileType
+        }
+
+        if (fileType == null) {
+            Messages.showMessageDialog(project, "No file type found", "Error", Messages.getErrorIcon())
+            return
+        }
+
+        val originalUrl = "https://ray.so/#language=${getFileLanguage(fileType)}&theme=raindrop&padding=16&background=true&code=$base64Code"
+
+        val url = originalUrl.replace("+", "-")
 
         try {
             Desktop.getDesktop().browse(URI(url))
